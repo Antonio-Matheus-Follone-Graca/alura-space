19dc7cb488c6960cfa20f5dfff4e147abfc11c5a
Antônio Matheus Follone Graça <matheus-follone@follone.com.br>
2023-02-04 22:42:44 -0300

    migrate da tabela fotografia

--- galeria/models.py
+++ galeria/models.py
@@ -3,3 +3,12 @@ from django.db import models
 # Create your models here.
 
 
+class Fotografia(models.Model):
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    legenda = models.CharField(max_length=150, null=False, blank=False)
+    descricao = models.TextField(null=False, blank=False)
+    foto = models.CharField(max_length=150, null=False, blank=False)
+
+    # ao chamar o objeto da classe retorna o nome da fotografia
+    def __str__(self):
+        return f"Fotografia [nome={self.nome}]"
