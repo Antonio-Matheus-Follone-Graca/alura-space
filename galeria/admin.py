9b04cab27c832cb0f20b7a22ec54697f2db832e3
Matheus <matheusfollone_trabalho@outlook.com>
2023-02-09 22:56:50 -0300

    configurações no admin e criação do campo categoria na models fotografia
    configurações no admin e criação do campo categoria na models fotografia

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from galeria.models import Fotografia
+
+# configurações sobre o django admin 
+
+# formatação da listagem de fotografias 
+class ListandoFotografias(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'legenda')  # mostrando dados da tabela fotografia pelos campos id, nome e legenda
+    list_display_links = ('id', 'nome') # mostrando links para o registro nos campos id e nome
+    search_fields = ('nome',)  # pesquisar pelo campo nome, o virgula vazio é obrigatório  
+ 
+ 
+
+# adicionando a opção da tabela fotografia, o segundo parametro e a classe de formatação de de fotografia
+admin.site.register(Fotografia, ListandoFotografias)
--- galeria/models.py
+++ galeria/models.py
@@ -4,10 +4,19 @@ from django.db import models
 
 
 class Fotografia(models.Model):
+    # lista com tuplas, pois o campo do tipo charfield só aceita formatos assim
+    opcoes_categoria = [
+        ('NEBULOSA', 'Nebulosa'),
+        ('ESTRELA', 'Estrela'),
+        ('GALÁXIA', 'Galáxia'),
+        ('PLANETA', 'Planeta')    
+    ]
     nome = models.CharField(max_length=100, null=False, blank=False)
     legenda = models.CharField(max_length=150, null=False, blank=False)
+    categoria = models.CharField(max_length = 100, choices= opcoes_categoria, default='')
     descricao = models.TextField(null=False, blank=False)
     foto = models.CharField(max_length=150, null=False, blank=False)
+
 
     # ao chamar o objeto da classe retorna o nome da fotografia
     def __str__(self):
