8ef369634affaed22218042ab1443d7605c473f2
Matheus <matheusfollone_trabalho@outlook.com>
2023-02-10 21:59:21 -0300

    Update views.py

--- galeria/views.py
+++ galeria/views.py
@@ -8,7 +8,7 @@ from galeria.models import Fotografia
 
 def index( request):
     # fazendo uma consulta dos dados da tabela
-    fotografias = Fotografia.objects.filter(publicada = True)
+    fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada = True)
 
     return render(request,'galeria/index.html', {"cards": fotografias})
 
