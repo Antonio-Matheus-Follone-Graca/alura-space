c7fb507622849c4fe75c6a183dfb7d052fad5a85
Matheus <matheusfollone_trabalho@outlook.com>
2023-02-20 00:52:17 -0300

    criação do app usuários

--- setup/urls.py
+++ setup/urls.py
@@ -22,6 +22,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     # importando as rotas do app galeria 
-    path('',include('galeria.urls'), name="index")
+    path('',include('galeria.urls')),
+    # importando as rotas do app usuarios
+    path('',include('usuarios.urls')),
     
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
---
+++ usuarios/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from usuarios.views import *
+from usuarios import views
+
+
+urlpatterns = [
+   path('')
+    
+]
